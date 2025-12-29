b76f4e383cad5df2a2a79c05c5bf92e5b0a8a8bb
CoinDevGamer <Izhanbhatti104@gmail.com>
2025-12-29 18:42:54 +0000

    Delete data-models.js

--- data-models.js
+++
@@ -1,7 +0,0 @@
-// List of models to show in portfolio
-window.MOLTENTIC_MODELS = [
-    { title: "Sample Model 1", src: "models/model1.glb", shadowIntensity: 0.8, exposure: 1.2 },
-    { title: "Sample Model 2", src: "models/model2.glb", shadowIntensity: 1, exposure: 1 },
-    { title: "Sample Model 3", src: "models/model3.glb", shadowIntensity: 1, exposure: 1 }
-  ];
-  
